2882c9e2100bfda97859e0c6ab62dc3c8af4c1f6
amjed96 <amjedbouallegui@gmail.com>
2024-12-22 20:37:01 +0100

    part-6: sqlite

--- app.py
+++ app.py
@@ -1,34 +1,87 @@
-import os
-from flask import Flask, render_template, request, url_for, flash, redirect
-from forms import CourseForm
+import os, sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-courses_list = [
-    {
-        'title': 'Python 101',
-        'description': 'Learn Python basics',
-        'price': 34,
-        'available': True,
-        'level': 'Beginner'
-    }
-]
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
-@app.route('/', methods=('GET', 'POST'))
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+@app.route('/')
 def index():
-    form = CourseForm()
-    if form.validate_on_submit():
-        courses_list.append({
-            'title': form.title.data,
-            'description': form.description.data,
-            'price': form.price.data,
-            'available': form.available.data,
-            'level': form.level.data
-        })
-        return redirect(url_for('courses'))
-    return render_template("index.html", form=form)
+    """ Index """
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
-@app.route('/courses/')
-def courses():
-    return render_template('courses.html', courses_list=courses_list)
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    """ Create """
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required !')
+        elif not content:
+            flash('Content is required !')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+@app.route('/edit/<int:id>/', methods=('GET', 'POST'))
+def edit(id):
+    """ Edit """
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required !')
+        elif not content:
+            flash('Content is required !')
+
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    
+    return render_template('edit.html', post=post)
+
+@app.route('/delete/<int:id>/', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+
+    conn = get_db_connection()
+
+    conn.execute('DELETE FROM posts WHERE id = ?',(id,))
+    conn.commit()
+    conn.close()
+
+    flash(f"{post['title']} was successfully deleted !")
+
+    return redirect(url_for('index'))
