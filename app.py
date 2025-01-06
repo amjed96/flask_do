b9f0b16d10ed5fdf45b25915f2ad408e7c9c0e27
amjed96 <amjedbouallegui@gmail.com>
2025-01-06 23:00:01 +0100

    part-10: sqlalchemy one to many

--- app.py
+++ app.py
@@ -1,88 +1,68 @@
 import os
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-from sqlalchemy.sql import func
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+           'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 db = SQLAlchemy(app)
 
-class Student(db.Model):
+class Post(db.Model):
+    """ Post entity """
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(100), nullable=False)
-    lastname = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(80), unique=True, nullable=False)
-    age = db.Column(db.Integer)
-    created_at = db.Column(
-        db.DateTime(timezone=True),
-        server_default=func.now()
-    )
-    bio = db.Column(db.Text)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
+    comments = db.relationship('Comment', backref='post')
 
     def __repr__(self):
-        return f'<Student {self.firstname}>'
-    
+        return f'<Post "{self.title}">'
+
+
+class Comment(db.Model):
+    """ Comment entity """
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return f'<Comment "{self.content[:20]}...">'
+
+
 @app.route('/')
 def index():
     """ Index """
-    students = Student.query.all()
-    return render_template('index.html', students=students)
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
 
-@app.route('/<int:student_id>/')
-def student(student_id):
-    """ Student """
-    student = Student.query.get_or_404(student_id)
-    return render_template('student.html', student=student)
-
-@app.route('/create/', methods=('GET', 'POST'))
-def create():
-    """ Create """
+@app.route('/<int:post_id>/', methods=('GET', 'POST'))
+def post(post_id):
+    """ Post """
+    post = Post.query.get_or_404(post_id)
     if request.method == 'POST':
-        student = Student(
-            firstname = request.form['firstname'],
-            lastname = request.form['lastname'],
-            email = request.form['email'],
-            age = request.form['age'],
-            bio = request.form['bio']
-        )
-        db.session.add(student)
+        comment = Comment(content=request.form['content'], post=post)
+        db.session.add(comment)
         db.session.commit()
+        return redirect(url_for('post', post_id=post.id))
 
-        return redirect(url_for('index'))
+    return render_template('post.html', post=post)
 
-    return render_template('create.html')
+@app.route('/comments/')
+def comments():
+    """ Comments """
+    comments = Comment.query.order_by(Comment.id.desc()).all()
+    return render_template('comments.html', comments=comments)
 
-@app.route('/edit/<int:student_id>/', methods=('GET','POST'))
-def edit(student_id):
-    """ Edit """
-    student = Student.query.get_or_404(student_id)
-
-    if request.method == 'POST':
-        student.firstname = request.form['firstname']
-        student.lastname = request.form['lastname']
-        student.email = request.form['email']
-        student.age = int(request.form['age'])
-        student.bio = request.form['bio']
-
-        db.session.add(student)
-        db.session.commit()
-
-        return redirect(url_for('index'))
-
-    return render_template('edit.html', student=student)
-
-@app.post('/delete/<int:student_id>/')
-def delete(student_id):
-    """ Delete """
-    student = Student.query.get_or_404(student_id)
-
-    db.session.delete(student)
+@app.post('/comments/delete/<int:comment_id>/')
+def delete_comment(comment_id):
+    """ Delete Comment """
+    comment = Comment.query.get_or_404(comment_id)
+    post_id = comment.post.id
+    db.session.delete(comment)
     db.session.commit()
-
-    return redirect(url_for('index'))
+    return redirect(url_for('post', post_id=post_id))
